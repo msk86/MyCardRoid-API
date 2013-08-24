9cd4b5cec06d418fb9f28dc95fa868846063a198
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-24 15:14:11 +0800

    create a simple api

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://ruby.taobao.org/'
+
+gem 'sinatra'
+gem 'thin'
+gem 'json'
+
+# Deploy with Capistrano
+#group :deploy do
+#  gem 'capistrano'
+#  gem "rvm-capistrano", ">= 1.3.0.rc11"
+#end
+
---
+++ app/main.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'json'
+require 'yaml'
+require 'sinatra'
+
+get '/upgrade.json' do
+  get_version_config
+end
+
+get '/upgrade-:language.json' do
+  get_version_config params[:language]
+end
+
+get '/database.json' do
+  get_database_config
+end
+
+get '/database-:language.json' do
+  get_database_config params[:language]
+end
+
+def get_database_config language = 'zh'
+  db_config = YAML::load(File.read('config/database.yml'))
+  db_config[language].to_json
+end
+
+def get_version_config language = 'zh'
+  version_config = YAML::load(File.read('config/version.yml'))
+  version_config[language].to_json
+end
+
+
