27f5b4ca40be1fee0a499c3490c33ead48d64622
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-26 15:39:08 +0800

    add config.ru for thin

--- app/main.rb
+++ app/main.rb
@@ -3,33 +3,40 @@ require 'json'
 require 'yaml'
 require 'sinatra'
 
-set :port, 8080
-set :public_folder, File.dirname(__FILE__) + '/public'
+class YGORoid < Sinatra::Application
 
-get '/upgrade.json' do
-  get_version_config
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
+  get '/upgrade.json' do
+    get_version_config
+  end
+
+  get '/upgrade-:language.json' do
+    get_version_config params[:language]
+  end
+
+  get '/database.json' do
+    get_database_config
+  end
+
+  get '/database-:language.json' do
+    get_database_config params[:language]
+  end
+
+  def get_database_config language = 'zh'
+    db_config = YAML::load(File.read('config/database.yml'))
+    db_config[language].to_json
+  end
+
+  def get_version_config language = 'zh'
+    version_config = YAML::load(File.read('config/version.yml'))
+    version_config[language].to_json
+  end
+
+
 end
 
-get '/upgrade-:language.json' do
-  get_version_config params[:language]
-end
 
-get '/database.json' do
-  get_database_config
-end
 
-get '/database-:language.json' do
-  get_database_config params[:language]
-end
-
-def get_database_config language = 'zh'
-  db_config = YAML::load(File.read('config/database.yml'))
-  db_config[language].to_json
-end
-
-def get_version_config language = 'zh'
-  version_config = YAML::load(File.read('config/version.yml'))
-  version_config[language].to_json
-end
 
 
---
+++ config.ru
@@ -0,0 +1,5 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('app',  __FILE__)
+
+run YGORoid.new
