810e8942ffa351a11c0869d3686ae19d62d1c423
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-26 22:28:40 +0800

    change order of create cdb

--- app/cdb.rb
+++ app/cdb.rb
@@ -9,27 +9,25 @@ def run
 end
 
 def create_cards_cdb file, language = 'zh'
-  p "Start create #{file}.cdb under #{language}..."
 
-  db = SQLite3::Database.new("app/public/#{language}/#{file}.cdb")
-
-  p "Start loading datas from my-card api..."
-
-  db.execute("CREATE TABLE datas(id integer primary key, ot integer, alias integet, setcode integer, type integer, atk integer, def integer, level integer, race integer, attribute integer, category integer)")
+  p "Loading datas from my-card api..."
   datas_uri = URI("http://my-card.in/cards.json")
   datas = JSON.parse Net::HTTP.get datas_uri
   count = datas.length
-  import_json_data db, 'datas', datas
 
   p "Start loading texts under #{language} from my-card api..."
-
-  db.execute("CREATE TABLE texts(id integer primary key, name varchar(128), desc varchar(1024), str1 varchar(256), str2 varchar(256), str3 varchar(256), str4 varchar(256), str5 varchar(256), str6 varchar(256), str7 varchar(256), str8 varchar(256), str9 varchar(256),			 str10 varchar(256), str11 varchar(256), str12 varchar(256), str13 varchar(256), str14 varchar(256), str15 varchar(256), str16 varchar(256) )")
   texts_uri = URI("http://my-card.in/cards_#{language}.json")
   texts = JSON.parse Net::HTTP.get texts_uri
+
+
+  p "Start create #{file}.cdb under #{language}..."
+  db = SQLite3::Database.new("app/public/#{language}/#{file}.cdb")
+  db.execute("CREATE TABLE datas(id integer primary key, ot integer, alias integet, setcode integer, type integer, atk integer, def integer, level integer, race integer, attribute integer, category integer)")
+  import_json_data db, 'datas', datas
+  db.execute("CREATE TABLE texts(id integer primary key, name varchar(128), desc varchar(1024), str1 varchar(256), str2 varchar(256), str3 varchar(256), str4 varchar(256), str5 varchar(256), str6 varchar(256), str7 varchar(256), str8 varchar(256), str9 varchar(256),			 str10 varchar(256), str11 varchar(256), str12 varchar(256), str13 varchar(256), str14 varchar(256), str15 varchar(256), str16 varchar(256) )")
   import_json_data db, 'texts', texts
 
   p "Update config in database.yml..."
-
   update_database_yml file, count, language
 
   p "Done!"
