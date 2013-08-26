2620e8740c017b43720661f04d4762bc839e5fe3
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-26 16:58:56 +0800

    add cron to server.sh

--- server.sh
+++ server.sh
@@ -4,3 +4,11 @@
 #server stop
 
 thin $1 -Cthin.yml
+
+if [ $YGOROID_API_ENV = production ] ; then
+    if [ $1 = start ] ; then
+        crontab cdb.cron
+    elif [ $1 = stop ] ; then
+        crontab -r
+    fi
+fi
