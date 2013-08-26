a23e7af3a02444f3e6338b94c3d6d72ab1d086ad
Fan Wenbo <xbfan@thoughtworks.com>
2013-08-27 07:49:54 +0800

    add log for cdb

--- app/cdb.rb
+++ app/cdb.rb
@@ -15,19 +15,19 @@ def create_cards_cdb file, language = 'zh'
   datas = JSON.parse Net::HTTP.get datas_uri
   count = datas.length
 
-  p "Start loading texts under #{language} from my-card api..."
+  p "Loading texts under #{language} from my-card api..."
   texts_uri = URI("http://my-card.in/cards_#{language}.json")
   texts = JSON.parse Net::HTTP.get texts_uri
 
 
-  p "Start create #{file}.cdb under #{language}..."
+  p "Creating #{file}.cdb under #{language}..."
   db = SQLite3::Database.new("app/public/#{language}/#{file}.cdb")
   db.execute("CREATE TABLE datas(id integer primary key, ot integer, alias integet, setcode integer, type integer, atk integer, def integer, level integer, race integer, attribute integer, category integer)")
   import_json_data db, 'datas', datas
   db.execute("CREATE TABLE texts(id integer primary key, name varchar(128), desc varchar(1024), str1 varchar(256), str2 varchar(256), str3 varchar(256), str4 varchar(256), str5 varchar(256), str6 varchar(256), str7 varchar(256), str8 varchar(256), str9 varchar(256),			 str10 varchar(256), str11 varchar(256), str12 varchar(256), str13 varchar(256), str14 varchar(256), str15 varchar(256), str16 varchar(256) )")
   import_json_data db, 'texts', texts
 
-  p "Update config in database.yml..."
+  p "Updating config in database.yml..."
   update_database_yml file, count, language
 
   p "Done!"
@@ -57,6 +57,10 @@ def update_database_yml file, count, language
   db_config[language]['count'] = count
   db_config[language]['upgrade_url'] = "#{db_config['base_url']}/#{language}/#{file}.cdb"
   File.write('config/database.yml', db_config.to_yaml)
+end
+
+def delete_previous_cdb
+
 end
 
 run
